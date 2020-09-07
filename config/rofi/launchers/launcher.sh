2f76af0b6e2b3ffc7812d37d7d69636507431f58
Jonas De <jonasd90@gmail.com>
2020-09-07 15:17:09 -0700

    update: rofi theme

--- config/rofi/launchers/launcher.sh
+++ config/rofi/launchers/launcher.sh
@@ -9,11 +9,20 @@
 #
 # >> Edit these files and uncomment the desired colors/style.
 #
-# style_icons_blur    style_icons_full    style_icons_rainbow    style_icons_rainbow_sidebar
-# style_icons_popup    style_normal    style_normal_grid    style_normal_grid_full    style_normal_grid_full_round
-# style_normal_grid_round    style_normal_purple    style_normal_purple_alt    style_normal_rainbow
-# style_normal_rainbow_sidebar
 
-style="style_normal"
+#style=style_icons_blur
+#style=style_icons_full
+#style=style_icons_rainbow
+#style=style_icons_rainbow_sidebar
+#style=style_icons_popup
+#style=style_normal
+#style=style_normal_grid
+#style=style_normal_grid_full
+#style=style_normal_grid_full_round
+style=style_normal_grid_round
+#style=style_normal_purple
+#style=style_normal_purple_alt
+#style=style_normal_rainbow
+#style=style_normal_rainbow_sidebar
 
 rofi -fake-transparency -no-lazy-grab -show drun -theme launchers/"$style".rasi
